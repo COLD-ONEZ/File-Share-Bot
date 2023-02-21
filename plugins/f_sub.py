59bce4fc1d3414c8c17c8dcd08983037f43d578f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-21 16:55:36 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -2,50 +2,38 @@ import asyncio
 from pyrogram import Client, enums
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from Script import script
 
 from database.join_reqs import JoinReqs
-from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
+from config import ADMINS, FORCE_MSG, REQ_CHANNEL, JOIN_REQS_DB, FORCE_SUB_CHANNEL as AUTH_CHANNEL
 
 from logging import getLogger
-
 logger = getLogger(__name__)
+
 INVITE_LINK = None
 db = JoinReqs
 
-async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="checksub"):
+async def ForceSub(bot: Client, update: Message):
     global INVITE_LINK
-    auth = ADMINS
-    if update.from_user.id in auth:
+    if update.from_user.id in ADMINS:
         return True
 
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
 
-    is_cb = False
-    if not hasattr(update, "chat"):
-        update.message.from_user = update.from_user
-        update = update.message
-        is_cb = True
-
     # Create Invite Link if not exists
     try:
         # Makes the bot a bit faster and also eliminates many issues realted to invite links.
         if INVITE_LINK is None:
-            invite_link = (await bot.create_chat_invite_link(
-                chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not JOIN_REQS_DB else REQ_CHANNEL),
-                creates_join_request=True if REQ_CHANNEL and JOIN_REQS_DB else False
-            )).invite_link
-            INVITE_LINK = invite_link
-            logger.info("Created Req link")
+            f_chat =  REQ_CHANNEL if REQ_CHANNEL else AUTH_CHANNEL
+            req_true = True if REQ_CHANNEL else False
+            create_invite = await bot.create_chat_invite_link(chat_id=f_chat, creates_join_request=req_true)
+            INVITE_LINK = create_invite.invite_link            
         else:
             invite_link = INVITE_LINK
-
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, update, file_id)
-        return fix_
-
+        fix_d = await ForceSub(bot, update)
+        return fix_d     
     except Exception as err:
         print(f"Unable to do Force Subscribe to {REQ_CHANNEL}\n\nError: {err}\n\n")
         await update.reply(text="Something went Wrong.")            
@@ -67,46 +55,41 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
         if not AUTH_CHANNEL:
             raise UserNotParticipant
         # Check if User is Already Joined Channel
-        user = await bot.get_chat_member(
-                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not db().isActive() else REQ_CHANNEL), 
-                   user_id=update.from_user.id
-               )
+        fcht_ids = AUTH_CHANNEL if not REQ_CHANNEL and not db().isActive() else REQ_CHANNEL
+        user = await bot.get_chat_member(chat_id=fcht_ids, user_id=update.from_user.id)                
         if user.status == enums.ChatMemberStatus.BANNED:
             await bot.send_message(chat_id=update.from_user.id, text="Sorry Sir, You are Banned to use me.")              
             return False
         else:
             return True
-
-    except UserNotParticipant:
-        text="""**Please Join My Updates Channel to use this Bot!**"""
-
+    except UserNotParticipant:        
         buttons = [[
             InlineKeyboardButton("📢 Request to Join Channel 📢", url=invite_link)
             ],[
-            InlineKeyboardButton(" 🔄 Try Again 🔄 ", callback_data=f"{mode}#{file_id}")
-        ]]
-                            
-        if not is_cb:
-            await update.reply(
-                text=text,
-                quote=True,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                parse_mode=enums.ParseMode.MARKDOWN,
-            )
+            InlineKeyboardButton(" 🔄 Try Again 🔄 ", url=f"https://t.me/{client.username}?start={update.command[1]}")
+        ]]         
+         
+        await update.reply(
+            text=FORCE_MSG.format(
+                first = update.from_user.first_name,
+                last = update.from_user.last_name,
+                username = None if not update.from_user.username else '@' + update.from_user.username,
+                mention = update.from_user.mention,
+                id = update.from_user.id
+            ),
+            quote=True,
+            reply_markup=InlineKeyboardMarkup(buttons),           
+        )
         return False
 
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, update, file_id)
-        return fix_
+        fix_d await ForceSub(bot, update, file_id)
+        return fix_d
 
     except Exception as err:
         print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
-        await update.reply(
-            text="Something went Wrong.",
-            parse_mode=enums.ParseMode.MARKDOWN,
-            disable_web_page_preview=True
-        )
+        await update.reply("Something went Wrong.")            
         return False
 
 
