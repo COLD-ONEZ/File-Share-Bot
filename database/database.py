143cdedb5fb150a7a826715ecf1a16c50f4714bc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-21 15:21:03 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,39 +1,37 @@
-#(©)CodeXBotz
-
-
-
-
-import pymongo, os
+import motor.motor_asyncio
 from config import DB_URI, DB_NAME
 
 
-dbclient = pymongo.MongoClient(DB_URI)
-database = dbclient[DB_NAME]
-
-
-user_data = database['users']
-
-
-
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    if found:
-        return True
-    else:
-        return False
-
-async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
-
-async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
         
-    return user_ids
+    def new_user(self, id):
+        return dict(_id = id)
+               
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'_id':int(id)})
+        return bool(user)
+    
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+            
+    async def get_all_users(self):
+        return self.col.find({})
+    
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'_id': int(user_id)})
 
-async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
+    
+db = Database(DB_URI, DB_NAME)
+
+
+
