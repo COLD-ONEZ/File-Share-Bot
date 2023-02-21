4c847eb9176b9a1e9d5c6e6eab181a9ccd40482e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-21 17:05:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,22 +5,22 @@ from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
-from helper_func import subscribed, encode, decode, get_messages
+from plugins.f_sub import ForceSub
+from config import ADMINS, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from helper_func import encode, decode, get_messages
 from database.database import db
 
 
 
-@Client.on_message(filters.command('start') & filters.private & subscribed)
+@Client.on_message(filters.command('start') & filters.private)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
-    if not await present_user(id):
-        try:
-            await add_user(id)
-        except:
-            pass
+    if not await db.is_user_exist(id):
+        await db.add_user(id)   
     text = message.text
-    if len(text)>7:
+    if len(text)>7: 
+        if not await ForceSub(client, message):    
+            return       
         try:
             base64_string = text.split(" ", 1)[1]
         except:
@@ -78,14 +78,10 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
+        reply_markup = InlineKeyboardMarkup([[
+            InlineKeyboardButton("😊 About Me", callback_data = "about"),
+            InlineKeyboardButton("🔒 Close", callback_data = "close")
+        ]])           
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
@@ -98,50 +94,14 @@ async def start_command(client: Client, message: Message):
             disable_web_page_preview = True,
             quote = True
         )
-        return
+        return 
 
     
-#=====================================================================================##
 
-WAIT_MSG = """"<b>Processing ...</b>"""
 
-REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
 
-#=====================================================================================##
 
-    
-    
-@Client.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "⚡️JOIN OUR CHANNEL⚡️",
-                url = client.invitelink)
-        ]
-    ]
-    try:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
-                )
-            ]
-        )
-    except IndexError:
-        pass
 
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
+
+
 
