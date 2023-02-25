72fd6cab67c8ea3ebd1e5025a2e3685481110a59
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-25 11:27:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, TG_BOT_TOKEN, CHANNEL_ID, PORT
 
 
 class Bot(Client):
@@ -19,7 +19,6 @@ class Bot(Client):
             workers=100,
             bot_token=TG_BOT_TOKEN
         )
-        self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
@@ -32,20 +31,17 @@ class Bot(Client):
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-            sys.exit()        
+            print(e)
         #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
+        print(f"@{usr_bot_me.username} is started")
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
-
+        print("bot stoped")
 
 bot=Bot()
 bot.run()
