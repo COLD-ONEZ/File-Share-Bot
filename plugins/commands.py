82be06e122ea3ed1f358ce4691f865e68756cf6f
Dᴀʀᴋ_Aɴɢᴇʟ <84849690+COLD-ONEZ@users.noreply.github.com>
2024-03-25 20:39:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,7 +56,7 @@ async def start_command(client: Client, message: Message):
         temp_msg = await message.reply("Please wait...")
         try:
             messages = await get_messages(client, ids)
-            await asyncio.sleep(30)
+            await messages.delete(30)
         except:
             await message.reply_text("Something went wrong..!")
             return
